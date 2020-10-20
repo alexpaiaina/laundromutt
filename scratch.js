35f3536bcb65f77df51025221fea5a6bf2cdabee
Alex Paiaina <alexpaiaina@gmail.com>
2020-10-20 10:28:05 -0600

    feature-app

--- scratch.js
+++ scratch.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function scratch() {
+
+  const handleChange = (e) => {
+    const { formData, setFormData } = useState({
+      username: '',
+      email: '', 
+      password: ''
+    })
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData(prevState => ({
+        ...prevState,
+        [name]: value
+      }))
+    }
+    
   return (
     <form>
-      <input type="text" name="" value={} onChange/>
+      <label> username
+        <input type="text" name="username" value={FormData, username} onChange={handleChange} />
+      </label>
+      <label> email
+        <input type="text" name="email" value={FormData, email} onChange={handleChange} />
+      </label>
+      <label> password
+        <input type="password" name="password" value={FormData, password} onChange={handleChange} />
+      </label>
       <input />
       <input />
     </form>
