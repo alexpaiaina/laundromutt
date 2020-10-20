1eed6e2bdcc0bb868ca0f7f592269c777f1b567f
Alex Paiaina <alexpaiaina@gmail.com>
2020-10-19 20:39:12 -0600

    feature-auth to development

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Dog.destroy_all
+Client.destroy_all
+User.destroy_all
 
-Dog.create!(dog_name: Finn, breed: mixed, img: , washes: 3)
-Dog.create!(dog_name: Kali, breed: havapoo, img: , washes: 5)
-Dog.create!(dog_name: Hazel, breed: havanese, img: , washes: 2)
-Dog.create!(dog_name: Kona, breed: havanese, img: , washes: 1)
+@user = User.create!(username: 'dragonflies', email: 'dragonflies@gmail.com', password: '123456')
+puts "#{User.count} users created"
+
+Dog.create!(dog_name: 'Finn', breed: 'mixed', img: , washes: 3)
+Dog.create!(dog_name: 'Kali', breed: 'havapoo', img: , washes: 5)
+Dog.create!(dog_name: 'Hazel', breed: 'havanese', img: , washes: 2)
+Dog.create!(dog_name: 'Kona', breed: 'havanese', img: , washes: 1)
+puts "#{Food.count} foods created"
+
+Client.create!(user_id: 'bobby', name: 'robert', email: 'bobby@email.com', phone: '8083459874', dog_name: 'Finn' )
+Client.create!(user_id: 'rich', name: 'richard', email: 'rich@email.com', phone: '8089228874', dog_name: 'Kali' )
+Client.create!(user_id: 'zhu', name: 'zhulieta', email: 'zhuzhu@email.com', phone: '4152277104', dog_name: 'Hazel' )
+Client.create!(user_id: 'eve', name: 'everest', email: 'himalaya@email.com', phone: '3102276893', dog_name: 'Kona' )
+puts "#{Client.count} clients created"
