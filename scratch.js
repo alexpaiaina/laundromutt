66f893d0b562c896b6bce3499aadb35017659218
Alex Paiaina <alexpaiaina@gmail.com>
2020-10-20 02:11:52 -0600

    feature-app to development

---
+++ scratch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function scratch() {
+  return (
+    <form>
+      <input type="text" name="" value={} onChange/>
+      <input />
+      <input />
+    </form>
+  )
+}
