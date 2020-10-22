7ef106795246a4a0683b07219e75a4e0a6cf2f87
Alex Paiaina <alexpaiaina@gmail.com>
2020-10-21 22:30:09 -0600

    feature-render to development

--- laundromutt-app/src/App.js
+++ laundromutt-app/src/App.js
@@ -1,26 +1,62 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { getAllClients } from './services/clients';
-import { Route } from 'react-router-dom';
-import Layout from ''
+import Login from './screens/Login';
+import Header from './layout/header/Header';
+import Footer from './layout/footer/Footer';
+
+import { loginUser, registerUser, removeToken, verifyUser } from './services/auth';
+
+import { Route, useHistory, Switch } from 'react-router-dom';
+import MainContainer from './containers/MainContainer';
 
 function App() {
-  useEffect(() => async () => {
-    const getClients = async () => {
-      cons clientsData = awit getAllClients();
-      console.log(clientsData)
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData)
     }
-    getClients();
-  }, []);
+    handleVerify();
+  }, [])
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/')
+  }
+
+  const handleRegister = async (registerData) => {
+    const userData = await registerUser(registerData);
+    setCurrentUser(userData);
+    history.push('/')
+  }
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+  }
+
   return (
-    <Layout>
-      <Routh path='/login'>
-        <Login path={auth/login} />
-      </Routh>
-      <Route path='/login' component={Login} />
-      <Route path='/login' render={(props) => (
-        <Login path={auth/login} {...props} />
-      )} />
+    <Layout
+      currentUser={currentUser}
+      handleLogout={handleLogout}
+    >
+      <Switch>
+        <Route path='/login'>
+          <Login handleLogin={handleLogin} />
+        </Route>
+        <Route path='/register'>
+          <Register handleRegister={handleRegister} />
+        </Route>
+        <Route path='/'>
+          <MainContainer />
+        </Route>
+      </Switch>
+
     </Layout>
   );
 }
